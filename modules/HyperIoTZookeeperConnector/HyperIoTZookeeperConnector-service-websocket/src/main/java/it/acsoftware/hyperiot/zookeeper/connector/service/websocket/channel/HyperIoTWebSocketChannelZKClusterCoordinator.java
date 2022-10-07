e4161a1ed542b1ee5d2297c7744fb29ca377a9f6
Aristide <aristide.cittadino@acsoftware.it>
2022-10-07 12:26:59 +0200

    fix channel participant path

--- modules/HyperIoTZookeeperConnector/HyperIoTZookeeperConnector-service-websocket/src/main/java/it/acsoftware/hyperiot/zookeeper/connector/service/websocket/channel/HyperIoTWebSocketChannelZKClusterCoordinator.java
+++ modules/HyperIoTZookeeperConnector/HyperIoTZookeeperConnector-service-websocket/src/main/java/it/acsoftware/hyperiot/zookeeper/connector/service/websocket/channel/HyperIoTWebSocketChannelZKClusterCoordinator.java
@@ -103,10 +103,7 @@ public class HyperIoTWebSocketChannelZKClusterCoordinator implements HyperIoTWeb
                         HyperIoTWebSocketChannel channel = loadChannel(path);
                         if (channel != null) {
                             channels.put(channel.getChannelId(), channel);
-                            if(!path.startsWith("/")){
-                                path = "/"+path;
-                            }
-                            String partecipantsPath = path + CHANNELS_PARTECIPANTS_PATH.replace("${channelId}", channel.getChannelId());
+                            String partecipantsPath = CHANNELS_PARTECIPANTS_PATH.replace("${channelId}", channel.getChannelId());
                             Map<HyperIoTWebSocketUserInfo, Set<HyperIoTWebSocketChannelRole>> partecipants = loadChannelsPartecipants(partecipantsPath);
                             partecipants.keySet().parallelStream().forEach(partecipant -> {
                                 channel.addPartecipantInfo(partecipant, partecipants.get(partecipant));
