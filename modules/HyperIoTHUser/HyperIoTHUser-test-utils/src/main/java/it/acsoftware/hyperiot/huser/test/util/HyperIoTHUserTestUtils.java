e292d937740ac3acfb10faa2a4c34214beaa9880
Aristide <aristide.cittadino@acsoftware.it>
2022-12-20 11:55:09 +0100

    adding test utils methods on user and permission

--- modules/HyperIoTHUser/HyperIoTHUser-test-utils/src/main/java/it/acsoftware/hyperiot/huser/test/util/HyperIoTHUserTestUtils.java
+++ modules/HyperIoTHUser/HyperIoTHUser-test-utils/src/main/java/it/acsoftware/hyperiot/huser/test/util/HyperIoTHUserTestUtils.java
@@ -1,9 +1,19 @@
 package it.acsoftware.hyperiot.huser.test.util;
 
+import it.acsoftware.hyperiot.base.api.HyperIoTAction;
 import it.acsoftware.hyperiot.base.test.util.HyperIoTTestUtils;
 import it.acsoftware.hyperiot.huser.api.HUserSystemApi;
+import it.acsoftware.hyperiot.huser.model.HUser;
+import it.acsoftware.hyperiot.huser.service.rest.HUserRestApi;
 import it.acsoftware.hyperiot.role.api.RoleSystemApi;
+import it.acsoftware.hyperiot.role.model.Role;
+import it.acsoftware.hyperiot.role.service.rest.RoleRestApi;
 import it.acsoftware.hyperiot.role.util.HyperIoTRoleConstants;
+
+import javax.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class HyperIoTHUserTestUtils {
     public static void truncateHUsers(HUserSystemApi systemApi) {
@@ -12,5 +22,48 @@ public class HyperIoTHUserTestUtils {
 
     public static void truncateRoles(RoleSystemApi systemApi) {
         HyperIoTTestUtils.truncateTables(systemApi, (role -> !role.getName().equalsIgnoreCase(HyperIoTRoleConstants.ROLE_NAME_REGISTERED_USER)));
+    }
+
+    public static HUser createHUser(HyperIoTAction action, HUserRestApi hUserRestApi, RoleRestApi roleRestApi, HUser loggedUser) {
+        String username = "TestUser";
+        List<Object> roles = new ArrayList<>();
+        HUser huser = createRandomHUser(username, false, true);
+        hUserRestApi.saveHUser(huser);
+        if (action != null) {
+            Role role = createRole(roleRestApi, loggedUser);
+            huser.addRole(role);
+            roleRestApi.saveUserRole(role.getId(), huser.getId());
+        }
+        return huser;
+    }
+
+    public static HUser registerAndActivateNewUser(HUserRestApi hUserRestApi) {
+        String username = "TestUser";
+        HUser huser = createRandomHUser(username, false, true);
+        hUserRestApi.register(huser);
+        String activationCode = huser.getActivateCode();
+        hUserRestApi.activate(huser.getEmail(), activationCode);
+        return huser;
+    }
+
+    public static Role createRole(RoleRestApi roleRestApi, HUser loggedUser) {
+        Role role = new Role();
+        role.setName("Role" + java.util.UUID.randomUUID());
+        role.setDescription("Description");
+        roleRestApi.saveRole(role);
+        return role;
+    }
+
+    private static HUser createRandomHUser(String username, boolean admin, boolean active) {
+        HUser huser = new HUser();
+        huser.setName("name");
+        huser.setLastname("lastname");
+        huser.setUsername(username + UUID.randomUUID().toString().replaceAll("-", ""));
+        huser.setEmail("testusername" + UUID.randomUUID().toString() + "@hyperiot.com");
+        huser.setPassword("passwordPass&01");
+        huser.setPasswordConfirm("passwordPass&01");
+        huser.setAdmin(admin);
+        huser.setActive(active);
+        return huser;
     }
 }
--- modules/HyperIoTPermission/HyperIoTPermission-test-utils/src/main/java/it/acsoftware/hyperiot/permission/test/util/HyperIoTPermissionTestUtil.java
+++ modules/HyperIoTPermission/HyperIoTPermission-test-utils/src/main/java/it/acsoftware/hyperiot/permission/test/util/HyperIoTPermissionTestUtil.java
@@ -1,8 +1,14 @@
 package it.acsoftware.hyperiot.permission.test.util;
 
+import it.acsoftware.hyperiot.base.api.HyperIoTAction;
+import it.acsoftware.hyperiot.huser.model.HUser;
 import it.acsoftware.hyperiot.permission.api.PermissionSystemApi;
+import it.acsoftware.hyperiot.permission.model.Permission;
+import it.acsoftware.hyperiot.permission.service.rest.PermissionRestApi;
 import it.acsoftware.hyperiot.role.api.RoleSystemApi;
+import it.acsoftware.hyperiot.role.model.Role;
 import it.acsoftware.hyperiot.role.util.HyperIoTRoleConstants;
+import org.junit.Assert;
 
 public class HyperIoTPermissionTestUtil {
     public static void dropPermissions(RoleSystemApi roleSystemApi, PermissionSystemApi permissionSystemApi) {
@@ -15,5 +21,28 @@ public class HyperIoTPermissionTestUtil {
                     });
                 });
 
+    }
+
+    private Permission utilGrantPermission(PermissionRestApi permissionRestApi, PermissionSystemApi permissionSystemApi, HUser huser, Role role, HyperIoTAction action, long resourceId) {
+        if (action == null) {
+            return null;
+        } else {
+            Permission testPermission = permissionSystemApi.findByRoleAndResourceName(role, action.getResourceName());
+            if (testPermission == null) {
+                Permission permission = new Permission();
+                permission.setName(resourceId + " assigned to huser_id " + huser.getId());
+                permission.setActionIds(action.getActionId());
+                permission.setEntityResourceName(action.getResourceName());
+                if (resourceId > 0)
+                    permission.setResourceId(resourceId);
+                permission.setRole(role);
+                testPermission = permission;
+            } else {
+                testPermission.addPermission(action);
+                permissionRestApi.updatePermission(testPermission);
+            }
+            Assert.assertTrue(huser.hasRole(role.getId()));
+            return testPermission;
+        }
     }
 }
